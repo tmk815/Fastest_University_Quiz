545f506563db9fee07cf4b6a4aaed7380aa73547
saboias21 <ep17112-1008@sti.chubu.ac.jp>
2018-12-06 23:17:36 +0900

    タイトルデザイン変更

--- src/title.cpp
+++ src/title.cpp
@@ -2,26 +2,31 @@
 
 //--------------------------------------------------------------
 void title::setup(){
-    /*button0.load("button_0.png"); //画像読み込み
-    button1.load("button_1.png"); //画像読み込み
-    title.load("title.png"); //画像読み込み*/
+    mecha60.load("mechanoarc.ttf", 60);
     timer = 0; //時間初期宣言
+    left = right = -10;
 }
 
 //--------------------------------------------------------------
 void title::update(){
     timer++; //1ずつ増加
+    w = ofGetWidth()/16;
+    h = ofGetHeight()/9;
 }
 
 //--------------------------------------------------------------
 void title::draw(){
     ofBackground(ofColor::black); //背景色設定
-    /*title.draw(ofGetWidth()/2-300, 0, 600, 100); //画面上部に配置（仮）
-    if(timer%100 < 80){
-        button0.draw(ofGetWidth()/2-250, ofGetHeight()/2-250, 500, 500); //画面中央に配置（仮）
-    }else{
-        button1.draw(ofGetWidth()/2-250, ofGetHeight()/2-250, 500, 500); //画面中央に配置（仮）
-    }*/
+    ofSetColor(ofColor::white);
+    mecha60.drawString("Buzzer Univercity Quiz", ofGetWidth()/2-500, 75);
+    ofNoFill();
+    for(int i=1; i<=7; i++){
+        ofDrawRectangle(w, h*i+40, w, h);
+        ofDrawRectangle(w*14, h*i+40, w, h);
+    }
+    ofFill();
+    ofDrawRectangle(w, h*left+40, w, h);
+    ofDrawRectangle(w*14, h*right+40, w, h);
 //----------３Dボタンの描画始
     float size = 40;
     int x=3, z=2;
@@ -48,7 +53,12 @@ void title::draw(){
 }
 
 void title::mousePressed(int x, int y, int button){
-
+    if(x/w==1 && 0<(y-40)/h && (y-40)/h<8){
+        left=(y-40)/h;
+    }
+    if(x/w==14 && 0<(y-40)/h && (y-40)/h<8){
+        right=(y-40)/h;
+    }
 }
 
 string title::getName(){
