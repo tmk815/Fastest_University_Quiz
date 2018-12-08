9620fdd94ea0db31c0821de423ef1fee6620395e
Monden Tomoki <monndenn15@gmail.com>
2018-12-08 22:32:39 +0900

    ジャンル、問題数を左上に表示

--- src/quiz.cpp
+++ src/quiz.cpp
@@ -13,6 +13,8 @@ void quiz::update(){
 //--------------------------------------------------------------
 void quiz::draw(){
     getSharedData().font.drawString(u8"クイズ画面", ofGetWidth()/2-100, ofGetHeight()/2);
+    getSharedData().font.drawString(ofToString(getSharedData().genre),0,30);
+    getSharedData().font.drawString(ofToString(getSharedData().number_of_questions),0,60);
 }
 
 void quiz::mousePressed(int x, int y, int button){
