3fe78070e844c8fd2f9c627bd48b393f84a31b3a
Monden Tomoki <monndenn15@gmail.com>
2018-12-08 16:28:04 +0900

    「クイズ画面」の表示(暫定的)

--- src/quiz.cpp
+++ src/quiz.cpp
@@ -12,7 +12,7 @@ void quiz::update(){
 
 //--------------------------------------------------------------
 void quiz::draw(){
-
+    getSharedData().font.drawString(u8"クイズ画面", ofGetWidth()/2-100, ofGetHeight()/2);
 }
 
 void quiz::mousePressed(int x, int y, int button){
