38f6d64f7be28dde611e60cfc3df604becf4e743
Monden Tomoki <monndenn15@gmail.com>
2018-12-10 16:21:04 +0900

    問題文の取得と表示(改行未対応)

--- src/quiz.cpp
+++ src/quiz.cpp
@@ -2,19 +2,28 @@
 
 //--------------------------------------------------------------
 void quiz::setup(){
-
+    quiz_xml.loadFile("DB.xml");
+    isans = true;
+    timer=0;
 }
 
 //--------------------------------------------------------------
 void quiz::update(){
-
+    string s = quiz_xml.getValue("q1:statement", "");
+    if(isans){
+        if(s.size() >= timer/6 && timer%6 == 0){
+            statement += s.substr(timer/6,1);
+        }
+    }
+    timer++;
 }
 
 //--------------------------------------------------------------
 void quiz::draw(){
-    getSharedData().font.drawString(u8"クイズ画面", ofGetWidth()/2-100, ofGetHeight()/2);
+    //getSharedData().font.drawString(u8"クイズ画面", ofGetWidth()/2-100, ofGetHeight()/2);
     getSharedData().font.drawString(ofToString(getSharedData().genre),0,30);
     getSharedData().font.drawString(ofToString(getSharedData().number_of_questions),0,60);
+    getSharedData().font.drawString(u8""+statement,0,ofGetHeight()/2);
 }
 
 void quiz::mousePressed(int x, int y, int button){
