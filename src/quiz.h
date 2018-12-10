aca72865c842df84404ce0960625e39fecb7ecd1
Monden Tomoki <monndenn15@gmail.com>
2018-12-10 16:21:22 +0900

    問題文の取得、表示用の変数を追加

--- src/quiz.h
+++ src/quiz.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "ofxState.h"
 #include "SharedData.h"
+#include "ofxXmlSettings.h"
 
 class quiz : public itg::ofxState<SharedData>{ //ofxStateを継承
 
@@ -11,4 +12,10 @@ class quiz : public itg::ofxState<SharedData>{ //ofxStateを継承
     string getName(); //状態の名前を返す関数
 
     ofTrueTypeFont font; //フォント用
+    ofxXmlSettings quiz_xml; //XMLの設定用
+    string statement; //問題文
+    string answer; //正解
+    bool isans; //正解したかどうかのフラグ
+    int timer; //文字数に使うtimer
+
 };
