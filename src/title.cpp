790ce52e3baa978f2153251d65c3b808bc49a13a
saboias21 <ep17112-1008@sti.chubu.ac.jp>
2018-12-11 11:41:33 +0900

    コメント追加のみ

--- src/title.cpp
+++ src/title.cpp
@@ -2,43 +2,43 @@
 
 //--------------------------------------------------------------
 void title::setup(){
-    mecha60.load("mechanoarc.ttf", 60);
-    mecha40.load("mechanoarc.ttf", 40);
+    mecha60.load("mechanoarc.ttf", 60); //
+    mecha40.load("mechanoarc.ttf", 40); //
     timer = 0; //時間初期宣言
-    left = right = -10;
+    left = right = -10; //
 }
 
 //--------------------------------------------------------------
 void title::update(){
     timer++; //1ずつ増加
-    w = ofGetWidth()/16;
-    h = ofGetHeight()/9;
+    w = ofGetWidth()/16; //
+    h = ofGetHeight()/9; //
 }
 
 //--------------------------------------------------------------
 void title::draw(){
     ofBackground(ofColor::black); //背景色設定
-    ofSetColor(ofColor::white);
-    mecha60.drawString("Buzzer Univercity Quiz", ofGetWidth()/2-500, 75);
-    ofNoFill();
-    for(int i=1; i<=7; i++){
+    ofSetColor(ofColor::white); //色指定：白
+    mecha60.drawString("Buzzer Univercity Quiz", ofGetWidth()/2-500, 75); //タイトル描画
+    ofNoFill(); //枠のみ
+    for(int i=1; i<=7; i++){ //判定位置に四角描画
         ofDrawRectangle(w, h*i+40, w, h);
         ofDrawRectangle(w*14, h*i+40, w, h);
     }
-    ofDrawRectangle(w*6, h*7+40, w*4, h);
-    mecha40.drawString("start", ofGetWidth()/2-75, h*7.75+40);
-    ofFill();
-    ofDrawRectangle(w, h*left+40, w, h);
-    ofDrawRectangle(w*14, h*right+40, w, h);
+    ofDrawRectangle(w*6, h*7+40, w*4, h); //start用の四角描画
+    mecha40.drawString("start", ofGetWidth()/2-75, h*7.75+40); //start描画
+    ofFill(); //塗りつぶす
+    ofDrawRectangle(w, h*left+40, w, h); //左側の選んだ四角描画
+    ofDrawRectangle(w*14, h*right+40, w, h); //右側の選んだ四角描画
 //----------３Dボタンの描画始
-    float size = 40;
-    int x=3, z=2;
-    cam.begin();
-    ofEnableDepthTest();
-    ofRotateY(timer);
-    ofRotateX(-30);
-    ofNoFill();
-    for(int i=0; i<x*2+1; i++){
+    float size = 40; //3Dの箱一つのサイズ
+    int x=3, z=2; //幅の半分、高さ
+    cam.begin(); //画面操作ON
+    ofEnableDepthTest(); //3dの動作正常化ON
+    ofRotateYDeg(timer); //y軸を回す
+    ofRotateXDeg(-30); //x軸を30度傾ける
+    ofNoFill(); //枠のみ
+    for(int i=0; i<x*2+1; i++){ //3dボタン描画
         for(int j=0; j<x*2+1; j++){
             for(int k=0; k<z; k++){
                 ofSetColor(ofColor::white);
@@ -50,21 +50,21 @@ void title::draw(){
             }
         }
     }
-    ofDisableDepthTest();
-    cam.end();
+    ofDisableDepthTest(); //3dの動作正常化OFF
+    cam.end(); //画面操作OFF
 //----------３Dボタンの描画終
 }
 
 void title::mousePressed(int x, int y, int button){
-    if(x/w==1 && 0<(y-40)/h && (y-40)/h<8){
+    if(x/w==1 && 0<(y-40)/h && (y-40)/h<8){ //ジャンル選択
         left=(y-40)/h;
         getSharedData().genre = left; //1~7
     }
-    if(x/w==14 && 0<(y-40)/h && (y-40)/h<8){
+    if(x/w==14 && 0<(y-40)/h && (y-40)/h<8){ //問題数選択
         right=(y-40)/h;
         getSharedData().number_of_questions = right; //1~7
     }
-    if((y-40)/h==7 && 5<x/w && x/w<10 && 0<left && 0<right){
+    if((y-40)/h==7 && 5<x/w && x/w<10 && 0<left && 0<right){ //開始ボタン判定
         changeState("quiz");
         left = right = -10;
     }
--- src/title.h
+++ src/title.h
@@ -13,9 +13,9 @@ class title : public itg::ofxState<SharedData>{ //ofxStateを継承
     ofImage button0; //画像表示のためのインスタンス変数
     ofImage button1; //画像表示のためのインスタンス変数
     ofImage title; //画像表示のためのインスタンス変数
-    ofEasyCam cam;
-    ofTrueTypeFont mecha60;
-    ofTrueTypeFont mecha40;
+    ofEasyCam cam; //3d操作のため
+    ofTrueTypeFont mecha60; //フォント使用のため
+    ofTrueTypeFont mecha40; //フォント使用のため
     int timer; //時間経過
-    int w, h, left, right;
+    int w, h, left, right; //画面の横幅、立幅、ジャンル、問題数
 };
