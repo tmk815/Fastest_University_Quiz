f891a7c56cdd52343698c13b9898f8aaabf756b5
Monden Tomoki <monndenn15@gmail.com>
2018-12-15 11:41:41 +0900

    回答機能を追加

--- src/quiz.cpp
+++ src/quiz.cpp
@@ -6,10 +6,16 @@ void quiz::setup(){
     isans_1p = false;
     isans_2p = false;
     timer=0;
+    player_ans = "";
+    getSharedData().correct_answers_1p = 0;
+    getSharedData().correct_answers_2p = 0;
+    position = 0;
+    srand(time_t(NULL));
 }
 
 //--------------------------------------------------------------
 void quiz::update(){
+    answer = quiz_xml.getValue("q1:ans", "");
     string s = quiz_xml.getValue("q1:statement", "");
     if((!isans_1p) && (!isans_2p)){
         if(s.length() >= timer/6 && timer%18 == 0){
@@ -24,29 +30,75 @@ void quiz::update(){
 
 //--------------------------------------------------------------
 void quiz::draw(){
-    //getSharedData().font.drawString(u8"クイズ画面", ofGetWidth()/2-100, ofGetHeight()/2);
-    getSharedData().font.drawString(ofToString(getSharedData().genre),0,30);
+    getSharedData().font.drawString(ofToString(getSharedData().correct_answers_1p),0,90);
+    getSharedData().font.drawString(ofToString(position),0,30);
     getSharedData().font.drawString(ofToString(getSharedData().number_of_questions),0,60);
     getSharedData().font.drawString(statement,0,ofGetHeight()/2);
 
     if(isans_1p || isans_2p){
         //回答欄
         ofDrawRectangle(150,400,90,90);
+        getSharedData().font.drawString(choice[0],180,460);
         ofDrawRectangle(350,400,90,90);
+        getSharedData().font.drawString(choice[1],380,460);
         ofDrawRectangle(550,400,90,90);
+        getSharedData().font.drawString(choice[2],580,460);
         ofDrawRectangle(750,400,90,90);
+        getSharedData().font.drawString(choice[3],780,460);
+
+        getSharedData().font.drawString(player_ans,ofGetWidth()/2,300);
     }
 }
 
 void quiz::mousePressed(int x, int y, int button){
     if(button == 2){changeState("title");} //右クリックでタイトルへ戻る
+    if(x>=150 && x<=240 && y>= 400 && y<= 490){
+        player_ans += choice[0];
+        makeChoices();
+    }else if(x>=350 && x<=440 && y>= 400 && y<= 490){
+        player_ans += choice[1];
+        makeChoices();
+    }else if(x>=550 && x<=640 && y>= 400 && y<= 490){
+        player_ans += choice[2];
+        makeChoices();
+    }else if(x>=750 && x<=840 && y>= 400 && y<= 490){
+        player_ans += choice[3];
+        makeChoices();
+    }
 }
 
 void quiz::keyReleased(int key){
     if(key == 'a' && !isans_2p){ //1Pが回答ボタンを押したとき
         isans_1p= true;   //回答フラグをtrueに
+        makeChoices();
     }else if(key == 'l' && !isans_2p){ //2Pが回答ボタンを押したとき
         isans_2p = true;   //回答フラグをtrueに
+        makeChoices();
+    }else if(key == 'r'){
+        isans_1p = isans_2p = false;
+    }
+}
+
+void quiz::makeChoices(){
+    checkAns();
+    for(int j=0;j<3;j++){
+        int i = rand() % 46;
+        choice[j] = hrgn[i];
+    }
+    choice[3] = answer.substr(position,1);
+    position += 1;
+}
+
+void quiz::checkAns(){
+    if(answer.length() <= player_ans.length()){
+        if(answer.compare(player_ans) == 0 && isans_1p){
+            getSharedData().correct_answers_1p++;
+        } else if(answer.compare(player_ans) == 0 && isans_2p){
+            getSharedData().correct_answers_2p++;
+        }
+        isans_1p = false;
+        isans_2p = false;
+        player_ans = "";
     }
 }
 
--- src/quiz.h
+++ src/quiz.h
@@ -10,13 +10,23 @@ class quiz : public itg::ofxState<SharedData>{ //ofxStateを継承
     void draw();
     void mousePressed(int x, int y, int button);
     void keyReleased(int key);
+    void makeChoices(); //ランダムな選択肢を作成する
+    void checkAns(); //正解かどうか確認
     string getName(); //状態の名前を返す関数
 
     ofTrueTypeFont font; //フォント用
     ofxXmlSettings quiz_xml; //XMLの設定用
     string statement; //問題文
     string answer; //正解
-    bool isans_1p,isans_2p; //正解したかどうかのフラグ
+    bool isans_1p,isans_2p; //回答中かどうかのフラグ
     int timer; //文字数に使うtimer
+    string player_ans; //回答
+    string choice[4]; //選択肢
+    char hrgn[46][4] = {
+            "あ","い","う","え","お","か","き","く","け","こ","さ","し","す","せ","そ",
+            "た","ち","つ","て","と","な","に","ぬ","ね","の","は","ひ","ふ","へ","ほ",
+            "ま","み","む","め","も","や","ゆ","よ","ら","り","る","れ","ろ","わ","を","ん" }; //あ～ん生成用
+    unsigned int position; //回答が何文字目か
+    int quiz_number; //何番目の問題か
 
 };
