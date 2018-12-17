b0edcc49e5ce7bd97c73ebeea02e01b00a9d0063
chidome <chidome2d@gmail.com>
2018-12-17 15:35:47 +0900

    ジャンル名、ジャンルデータファイル名追加

--- src/SharedData.h
+++ src/SharedData.h
@@ -1,9 +1,11 @@
-#pragma once
+﻿#pragma once
 
 class SharedData
 {
 public:
     int genre; //選択したジャンル
+	string genrename[7]={"データベース","English"};
+	string genrefile[7]={"DB","English"};
     int number_of_questions; //問題数
     int correct_answers_1p; //1P正答数
     int correct_answers_2p; //2P正答数
