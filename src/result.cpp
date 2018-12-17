000c4b4fc82f43f8266503dd92a4425938a7a2db
chidome <chidome2d@gmail.com>
2018-12-17 16:46:08 +0900

    「正答率、誤答率、未答率」表示完成

--- src/result.cpp
+++ src/result.cpp
@@ -8,28 +8,41 @@ void result::setup(){
 
 //--------------------------------------------------------------
 void result::update(){
-
-
+	/*getSharedData().correct_answers_1p=2;
+	getSharedData().correct_answers_2p=1;
+	getSharedData().incorrect_answers_1p=1;
+	getSharedData().incorrect_answers_2p=2;
+	getSharedData().number_of_questions=3;
+	getSharedData().Unanswered_numbers=2;*/
+	if(getSharedData().correct_answers_1p==getSharedData().correct_answers_2p)winner="ＤＲＡＷ";
+	if(getSharedData().correct_answers_1p>getSharedData().correct_answers_2p)winner="勝者：1P";
+	if(getSharedData().correct_answers_1p<getSharedData().correct_answers_2p)winner="勝者：2P";
+	correct_ratio_1p=getSharedData().correct_answers_1p/(double)getSharedData().number_of_questions;
+	correct_ratio_2p=getSharedData().correct_answers_2p/(double)getSharedData().number_of_questions;
+	incorrect_ratio_1p=getSharedData().incorrect_answers_1p/(double)(getSharedData().correct_answers_1p+getSharedData().incorrect_answers_1p);
+	incorrect_ratio_2p=getSharedData().incorrect_answers_2p/(double)(getSharedData().correct_answers_2p+getSharedData().incorrect_answers_2p);
+	unanswered_ratio=getSharedData().Unanswered_numbers/(double)getSharedData().number_of_questions;
 }
 
 //--------------------------------------------------------------
 void result::draw(){
 	ofSetColor(ofColor::white);
 	ofDrawLine(ofGetWidth()/2,ofGetHeight()/6*2,ofGetWidth()/2,ofGetHeight()/6*4);//ボーダーライン
-	getSharedData().font.drawString("ジャンル：数学", ofGetWidth()/2-fontsize*15,ofGetHeight()/12*1);
-	getSharedData().font.drawString("勝者：1P", ofGetWidth()/2-fontsize*7,ofGetHeight()/12*3);
-	getSharedData().font.drawString("1P正答率：100%", ofGetWidth()/4-fontsize*7,ofGetHeight()/12*5);
-	getSharedData().font.drawString("1P誤答率：100%", ofGetWidth()/4-fontsize*7,ofGetHeight()/12*7);
-	getSharedData().font.drawString("2P正答率：100%", ofGetWidth()/4*3-fontsize*7,ofGetHeight()/12*5);
-	getSharedData().font.drawString("2P誤答率：100%", ofGetWidth()/4*3-fontsize*7,ofGetHeight()/12*7);
-	getSharedData().font.drawString("未答率：50%", ofGetWidth()/2-fontsize*5.5,ofGetHeight()/12*9);
+	getSharedData().font.drawString("ジャンル："+getSharedData().genrename[getSharedData().genre], ofGetWidth()/2-fontsize*9,ofGetHeight()/12*1);
+	getSharedData().font.drawString(winner, ofGetWidth()/2-fontsize*4,ofGetHeight()/12*3);
+	getSharedData().font.drawString("1P正答率："+ofToString(round(correct_ratio_1p*1000.0)/10.0)+"%", ofGetWidth()/4*1-fontsize*6.5,ofGetHeight()/12*5);
+	getSharedData().font.drawString("1P誤答率："+ofToString(round(incorrect_ratio_1p*1000.0)/10.0)+"%", ofGetWidth()/4*1-fontsize*6.5,ofGetHeight()/12*7);
+	getSharedData().font.drawString("2P正答率："+ofToString(round(correct_ratio_2p*1000.0)/10.0)+"%", ofGetWidth()/4*3-fontsize*6.5,ofGetHeight()/12*5);
+	getSharedData().font.drawString("2P誤答率："+ofToString(round(incorrect_ratio_2p*1000.0)/10.0)+"%", ofGetWidth()/4*3-fontsize*6.5,ofGetHeight()/12*7);
+	getSharedData().font.drawString("未答率："+ofToString(round(unanswered_ratio*1000.0)/10.0)+"%", ofGetWidth()/2-fontsize*7,ofGetHeight()/12*9);
 	buttonw=300;
 	buttonh=ofGetHeight()/12;
 	buttonx=ofGetWidth()/2-buttonw/2;
 	buttony=ofGetHeight()/12*10;
+	ofFill();
 	ofDrawRectRounded(buttonx,buttony,0,buttonw,buttonh,10);
 	ofSetColor(ofColor::red);
-	getSharedData().font.drawString("Return to Title", ofGetWidth()/2-fontsize*7.5,ofGetHeight()/12*11);
+	getSharedData().font.drawString("Return to Title", ofGetWidth()/2-fontsize*5,ofGetHeight()/12*11);
 }
 
 void result::mousePressed(int x, int y, int button){
--- src/result.h
+++ src/result.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 #include "ofxState.h"
 #include "SharedData.h"
+#include "math.h"
 
 class result : public itg::ofxState<SharedData>{ //ofxStateを継承
 
@@ -9,6 +10,13 @@ class result : public itg::ofxState<SharedData>{ //ofxStateを継承
     void draw();
     void mousePressed(int x, int y, int button);
     string getName(); //状態の名前を返す関数
-	double fontsize=14;
+	double fontsize=22;
 	int buttonx,buttony,buttonw,buttonh;
+	string winner="ＤＲＡＷ";
+	double correct_ratio_1p;
+	double correct_ratio_2p;
+	double incorrect_ratio_1p;
+	double incorrect_ratio_2p;
+	double unanswered_ratio;
+
 };
