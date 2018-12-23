789dd3abb1ab6177407da0dcaef2f915d13e6be5
saboias21 <ep17112-1008@sti.chubu.ac.jp>
2018-12-23 10:23:15 +0900

    xml追加

--- src/SharedData.h
+++ src/SharedData.h
@@ -4,8 +4,8 @@ class SharedData
 {
 public:
     int genre; //選択したジャンル
-	string genrename[7]={"データベース","English"};
-	string genrefile[7]={"DB","English"};
+    string genrename[7]={"データベース","English","情報理論","日本史","OS","世界史"};
+    string genrefile[7]={"DB","English","johoriron","nihonshi", "OS", "sekaishi"};
     int number_of_questions; //問題数
     int correct_answers_1p; //1P正答数
     int correct_answers_2p; //2P正答数
